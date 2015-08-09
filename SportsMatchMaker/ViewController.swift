7339a5590032f3a4cc70e4004eeefa9d3b6e19c8
Indo Ty <IndoTy@192-168-1-106.tpgi.com.au>
2015-08-09 19:28:46 +1000

    Initial upload.

--- SportsMatchMaker/ViewController.swift
+++ SportsMatchMaker/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate, CLLocationManagerDelegate{
+
+    @IBOutlet var webView: UIWebView!
+    
+    var indicator: UIActivityIndicatorView!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray);
+        indicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        indicator.center = view.center;
+        view.addSubview(indicator);
+        indicator.bringSubviewToFront(view);
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true;
+        
+        
+        let url = NSURL (string: "http://mvc5identity.azurewebsites.net");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.scrollView.bounces = false;
+        webView.delegate = self;
+        //webView.scrollView.delegate = self;
+        //webView.scrollView.showsHorizontalScrollIndicator = false;
+        webView.scalesPageToFit = true;
+        webView.loadRequest(requestObj);
+        
+        locationManager = CLLocationManager();
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.requestAlwaysAuthorization();
+        locationManager.startUpdatingLocation();
+    
     }
 
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if (scrollView.contentOffset.x != 0) {
+            scrollView.contentOffset = CGPointMake(0, scrollView.contentOffset.y);
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        indicator.stopAnimating();
+        print("Couldn't load content.");
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        indicator.startAnimating();
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        indicator.stopAnimating();
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.startUpdatingLocation();
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            locationManager.stopUpdatingLocation();
+        }
+    }}
 
